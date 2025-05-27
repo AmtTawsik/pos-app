3bb1f263f23ce9eb92e5d39cc9b5ad1fab619b3c
Abdullah Al Mubin <87541267+AmtTawsik@users.noreply.github.com>
2025-05-27 21:30:09 +0600

    Deploy frontend to Netlify

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  },
   server: {
     port: 5173,
     proxy: {
@@ -14,4 +17,4 @@ export default defineConfig({
       }
     }
   }
-})
+});
